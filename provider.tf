01bafaad400b3f12fe3ac5f0226d4db196238007
jibin <perseverancejbt@hotmail.com>
2025-11-09 23:35:28 +0800

    Assignment 3.6

--- provider.tf
+++ provider.tf
@@ -1,17 +1,20 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-  archive = {
+
+    archive = {
       source  = "hashicorp/archive"
       version = "~> 2.4"
-  }
+    }
   }
 }
 
-# Configure the AWS Provider
-# provider "aws" {
-#  region = "us-east-1"
-#}
+provider "aws" {
+  region = "us-east-1"
+}
+
