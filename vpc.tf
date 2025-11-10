c64382b2279a6e898e63bc1772d9fb39a77296ee
JBTTT <perseverancejbt@hotmail.com>
2025-11-10 23:26:38 +0800

    Assignment3.6

--- vpc.tf
+++ vpc.tf
@@ -6,8 +6,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
-  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  private_subnets = ["10.0.150.0/24", "10.0.160.0/24", "10.0.170.0/24"]
+  public_subnets  = ["10.0.151.0/24", "10.0.161.0/24", "10.0.171.0/24"]
 
   enable_vpn_gateway = true
 
